6ee2169f566ea0ba37eb9604a8289748d5b391ae
jeiss <jeissoncortes02@outlook.com>
2022-10-05 21:37:04 -0500

    prueba con gradle

---
+++ src/main/java/com/example/testcigardle/TestCiGardleApplication.java
@@ -0,0 +1,13 @@
+package com.example.testcigardle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCiGardleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCiGardleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testcigardle/TestCiGardleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testcigardle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCiGardleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
