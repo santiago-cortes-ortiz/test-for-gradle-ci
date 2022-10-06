abc7a8f4e5ce085bc0ef0d99bf690895dcf9b136
jeiss <jeissoncortes02@outlook.com>
2022-10-05 21:50:29 -0500

    prueba con gradle

---
+++ src/test/java/com/example/testcigardle/TestBaicos.java
@@ -0,0 +1,16 @@
+package com.example.testcigardle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class TestBaicos {
+
+    @Test
+    void test() {
+        var a = "a";
+        assertEquals("a", a);
+    }
+}
